8a3c3cbd1cc4386864377e771c44b1b04bf09ca1
NewMaoCompany <lujunzhang01@gmail.com>
2026-06-11 17:58:32 -0400

    Remove 5 message limit text from PointsPage.tsx

--- pages/PointsPage.tsx
+++ pages/PointsPage.tsx
@@ -241,7 +241,7 @@ export const PointsPage = () => {
                                 "Receive a reply to your message: earn 10 NMS Points",
                                 "Add a new friend on the platform: earn 10 NMS Points (both users)",
                                 "Points from community engagement appear as a 'Claim Gold' button in the Forum",
-                                "Limit: Only the first 5 messages you post each day are eligible to earn points from likes and replies.",
+
                                 "Limit: Repeatedly adding and removing the same friend will not generate points (to prevent farming).",
                                 "Note: self-likes do not generate NMS Points",
                             ]}
